4581a918e48085f0f6d65239e56babde5ee1f477
Ivan Proskurnin <43935004+tragedyHub@users.noreply.github.com>
2021-06-23 19:42:14 +0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {Switch, Route} from "react-router-dom"
+import Home from "./components/Home/Home"
 
 const App = () => {
   return (
     <div>
+      <Switch>
+        <Route path="/" exact render={() => <Home />}></Route>
+      </Switch>
     </div>
   )
 }
